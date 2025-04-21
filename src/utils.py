6ac502139ecc9d430efe5caf9340e5773fb3b05e
Anthoniez <jayfernandojay@gmail.com>
2025-04-21 17:44:22 +1000

    Edits to  gitignore

--- src/utils.py
+++ src/utils.py
@@ -22,31 +22,35 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-            para=param[list(models.keys())[i]]
+        for model_name, model in models.items():
+            try:
+                print(f"Evaluating model: {model_name}")
+                model_params = param.get(model_name, {})
 
-            gs = GridSearchCV(model,para,cv=3)
-            gs.fit(X_train,y_train)
+                if model_params:
+                    gs = GridSearchCV(model, model_params, cv=3)
+                    gs.fit(X_train, y_train)
 
-            model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
+                    model.set_params(**gs.best_params_)
 
-            #model.fit(X_train, y_train)  # Train model
+                # Even if GridSearchCV wasn't used, fit the model
+                model.fit(X_train, y_train)
 
-            y_train_pred = model.predict(X_train)
+                y_train_pred = model.predict(X_train)
+                y_test_pred = model.predict(X_test)
 
-            y_test_pred = model.predict(X_test)
+                train_model_score = r2_score(y_train, y_train_pred)
+                test_model_score = r2_score(y_test, y_test_pred)
 
-            train_model_score = r2_score(y_train, y_train_pred)
+                report[model_name] = test_model_score
 
-            test_model_score = r2_score(y_test, y_test_pred)
-
-            report[list(models.keys())[i]] = test_model_score
+            except Exception as inner_e:
+                print(f"[Warning] Model {model_name} failed during evaluation: {inner_e}")
+                continue
 
         return report
 
